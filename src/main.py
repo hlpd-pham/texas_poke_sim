d4fccc23fa73890774226ce280560d69d8824ae0
Hoa Pham <hoa.phamlethai@gmail.com>
2024-03-20 09:15:11 -0700

    add some game sense

--- src/main.py
+++ src/main.py
@@ -11,16 +11,31 @@ if __name__ == "__main__":
         filemode="w",
         format="%(asctime)s - %(levelname)s - %(filename)s:%(lineno)s - %(funcName)s - %(message)s",
     )
-    num_players = 5
-    print(f"start game for {num_players} players")
-    game = Game(num_players=num_players, debug=True)
+    num_players = 1
+    players_and_dealer = num_players + 1
+    print(f"start game-----------------------------------")
+    game = Game(num_players=players_and_dealer, debug=True)
+    dealer_id = list(game.players.keys())[-1]
 
-    for deal_type in [CardDealAmount.FLOP, CardDealAmount.TURN, CardDealAmount.RIVER]:
-        game.dealing_to_board(deal_type)
+    player_ids = set([p_id for p_id in game.players.keys()]).remove(dealer_id)
+    print("player cards")
+    for p in game.players.values():
+        print(to_string(p.cards))
 
+    print("dealer cards:", to_string(game.players[dealer_id].cards))
+
+    decision = input("Player make decision: ([P]lay, [F]old) > ")
+
+    game.dealing_to_board(CardDealAmount.FLOP)
     print("board")
     print(to_string(game.get_board()))
-    print()
+
+    decision = input("Player make decision: ([P]lay, [F]old) > ")
+
+    game.dealing_to_board(CardDealAmount.TURN)
+    game.dealing_to_board(CardDealAmount.RIVER)
+    print("board")
+    print(to_string(game.get_board()))
 
     print("hand results")
     game._evaluating_player_hands()
@@ -28,5 +43,12 @@ if __name__ == "__main__":
         print(f"id:{player.id}, {to_string(player.cards)}, {player.hand_result}")
 
     print("winners")
-    for w in game.find_winners():
+    winners = game.find_winners()
+    for w in winners:
         print(f"id:{w.id}, {to_string(w.cards)}, {w.hand_result}")
+
+    if dealer_id in [w.id for w in winners]:
+        print("dealer wins")
+    else:
+        print("players win")
+    print(f"end game-----------------------------------")
